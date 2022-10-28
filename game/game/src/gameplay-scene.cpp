7b8a3827da98cf1151954c272e6641cca1bac15b
Iliyana Michevska <INMichevska20@codingburgas.bg>
2022-10-28 23:08:51 +0300

    Add collision checks

--- game/game/src/gameplay-scene.cpp
+++ game/game/src/gameplay-scene.cpp
@@ -5,9 +5,15 @@ void renderGameplayScene(Vector2* rabbitPosition, int* rabbitDirection, int fram
 
             Vector2 temp = *rabbitPosition;
 
-            if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) {
-                temp.x += 2.0f;
-                *rabbitPosition = temp;
+            int radius = 92;
+
+            if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) 
+            {
+                if (temp.x < (GetScreenWidth() - radius)) 
+                {
+                    temp.x += 2.0f;
+                    *rabbitPosition = temp;
+                }
 
                 if (frame == 1) {
                     DrawTextureV(*right, *rabbitPosition, WHITE);
@@ -18,49 +24,68 @@ void renderGameplayScene(Vector2* rabbitPosition, int* rabbitDirection, int fram
                 *rabbitDirection = 1;
 
             }
-            else if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) {
-                temp.x -= 2.0f;
-                *rabbitPosition = temp;
+
+            else if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) 
+            {
+                if (temp.x != 8)
+                {
+                    temp.x -= 2.0f;
+                    *rabbitPosition = temp;
+                }
 
                  if (frame == 1) {
                     DrawTextureV(*left, *rabbitPosition, WHITE);
-                }
+                    }
                 else {
                     DrawTextureV(*left2, *rabbitPosition, WHITE);
                 }
                 *rabbitDirection = 2;
             }   
-            else if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) {
-                temp.y -= 2.0f;
-                *rabbitPosition = temp;
 
+            else if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) 
+            {  
+                if(temp.y != 8)
+                {
+                    temp.y -= 2.0f;
+                    *rabbitPosition = temp;
+                }
+                    
                 DrawTextureV(*back, *rabbitPosition, WHITE);
                 *rabbitDirection = 3;
             }
 
-            else if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)) {
+            else if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S))
+            {
+                if (temp.y < (GetScreenHeight() - radius))
+                {
+                    temp.y += 2.0f;
+                    *rabbitPosition = temp;
+                }
 
-            temp.y += 2.0f;
-            *rabbitPosition = temp;
-
-            DrawTextureV(*front, *rabbitPosition, WHITE);
-            *rabbitDirection = 4;
-
+                DrawTextureV(*front, *rabbitPosition, WHITE);
+                *rabbitDirection = 4;
             }
-        else {
-           switch (*rabbitDirection) {
-           case 1:
+
+            else {
+
+            switch (*rabbitDirection) {
+
+            case 1:
             DrawTextureV(*right, *rabbitPosition, WHITE);
             break;
+
             case 2:
             DrawTextureV(*left,* rabbitPosition, WHITE);
             break;
+
             case 3:
             DrawTextureV(*back, *rabbitPosition, WHITE);
             break;
+
             case 4:
             DrawTextureV(*front,* rabbitPosition, WHITE);
             break;
+            
             default:
             DrawTextureV(*front,* rabbitPosition, WHITE);
             break;
