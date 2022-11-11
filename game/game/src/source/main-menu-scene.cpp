3c3356e6135eaa0e1ac1f6f977024dd3dc51862e
Iliyana Michevska <INMichevska20@codingburgas.bg>
2022-11-11 20:12:27 +0200

    Make comments and modify indentation style

--- game/game/src/source/main-menu-scene.cpp
+++ game/game/src/source/main-menu-scene.cpp
@@ -1,26 +1,36 @@
 #include "../headers/include.hpp"
 #include "../headers/main-menu-scene.hpp"
 
-void renderMainMenu(Texture2D mainBackground, Texture2D button, Texture2D buttonHover, Vector2 mousePos, GameScreen* state) {
+// Main menu function
+void renderMainMenu(Texture2D mainBackground, Texture2D button, Texture2D buttonHover, Vector2 mousePos, GameScreen* state) 
+{
+    // Draw the texture of the menu
     DrawTexture(mainBackground,0, 0, WHITE);
     DrawTexture(button, 280, 312, WHITE);
 
     Rectangle btnCollide = {280, 312, 360, 70};
 
-     if (CheckCollisionPointRec(mousePos, btnCollide)) {
-         //UnloadTexture(button);
-         DrawTexture(buttonHover, 280, 312, WHITE);
+    // Check if the cursor is over menu options
+    if (CheckCollisionPointRec(mousePos, btnCollide)) 
+    {
+        // UnloadTexture(button);
+        DrawTexture(buttonHover, 280, 312, WHITE);
 
-        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) 
+        {
             *state = LEVELSELECT;
         }
-     }
-    if (IsKeyReleased(KEY_ENTER)) {
+    }
+    
+    if (IsKeyReleased(KEY_ENTER)) 
+    {
         *state = LEVELSELECT;
     }
 }
 
-void unloadMenu(Texture2D asset1, Texture2D asset2, Texture2D asset3) {
+// Unload textures from the menu
+void unloadMenu(Texture2D asset1, Texture2D asset2, Texture2D asset3) 
+{
     UnloadTexture(asset1);
     UnloadTexture(asset2);
     UnloadTexture(asset3);
--- game/game/src/source/main.cpp
+++ game/game/src/source/main.cpp
@@ -6,13 +6,17 @@
 int main(void)
 {
     // Initialization
-    const int screenWidth = 960;   //Window size initialization
+
+    // Window size initialization
+    const int screenWidth = 960;   
     const int screenHeight = 524;
 
-    InitWindow(screenWidth, screenHeight, "Eurobunnea");    //Window initialization
+    // Window initialization
+    InitWindow(screenWidth, screenHeight, "Eurobunnea");    
     GameScreen currentScreen = TITLE;
 
-    Texture2D mainBackground = LoadTexture("../src/assets/main-menu-bg.png");   //Assets initialization
+    // Assets initialization
+    Texture2D mainBackground = LoadTexture("../src/assets/main-menu-bg.png");   
     Texture2D map = LoadTexture("../src/assets/level-select-map.png");
     Texture2D tutorialBackground = LoadTexture("../src/assets/tutorial-bg.png");
     Texture2D franceBackground = LoadTexture("../src/assets/france-bg.png");
@@ -45,96 +49,115 @@ int main(void)
     Texture2D right = LoadTexture("../src/assets/right.png");
     Texture2D right2 = LoadTexture("../src/assets/right-2.png");
     Texture2D rightWalk2 = LoadTexture("../src/assets/right-walk-2.png");
+    
+    // Initilize the necessary variables
 
+    // X and Y position of the rabbit
     Vector2 rabbitPosition = { 440, 420 };
     
     Color firstColor = WHITE;
     Color secondColor = WHITE;
+
+    int frame = 0;
+    int franceDialogue = 0;
     int rabbitDirection = 0;
     int countryChoice = 0;
+    int franceDialogueCounter = 0;
     float timer = 0.0f;
-    int frame = 0;
-    bool completedTotal[5] = {0, 0, 0, 0, 0};
-
-    int franceDialogue = 0;
     bool isFrench = 1;
     bool isFrenchTalking = 0;
-    int franceDialogueCounter = 0;
+    bool completedTotal[5] = {0, 0, 0, 0, 0};
+
     questionPts franceQuestions;
     questionPts tempCorrect;
     
-    SetTargetFPS(60);   // Set game fps
+    // Set game FPS
+    SetTargetFPS(60);   
 
-    while (!WindowShouldClose())    // Detect window close + main game loop
+    // Detect window close and main game loop
+    while (!WindowShouldClose())    
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-           Vector2 mousePos = GetMousePosition();
-           timer += GetFrameTime();
-           if (timer >= 0.4f) {
+        // Update the variables here
+        Vector2 mousePos = GetMousePosition();
+        timer += GetFrameTime();
+        if (timer >= 0.4f) 
+        {
             timer = 0.0f;
             frame += 4;
-           }
+        }
 
-           frame = frame % 3;
-        //----------------------------------------------------------------------------------
+        frame = frame % 3;
 
-        // Draw
-        //----------------------------------------------------------------------------------
+        // Begin drawing
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
+        //Clear the colour of the background
+        ClearBackground(RAYWHITE);
 
-            switch(currentScreen){
-                case TITLE:
-                renderMainMenu(mainBackground, startBtn, startBtnHover, mousePos, &currentScreen);
-                break;
+        //Check what is the current state of the screen
+        switch(currentScreen)
+        {
+            case TITLE:
+            renderMainMenu(mainBackground, startBtn, startBtnHover, mousePos, &currentScreen);
+            break;
 
-                case LEVELSELECT:
-                DisableCursor();
-                unloadMenu(mainBackground, startBtn, startBtnHover);
-                renderLevelSelection(map, star, &countryChoice, &currentScreen);
+            case LEVELSELECT:
+            DisableCursor();
+            unloadMenu(mainBackground, startBtn, startBtnHover);
+            renderLevelSelection(map, star, &countryChoice, &currentScreen);
 
-                (completedTotal[0]) ? DrawText("-Tutorial Completed", 30, 110, 20, GRAY) : DrawText("", 30, 110, 20, GRAY);
-                (completedTotal[1]) ? DrawText("-Italy Completed", 30, 110, 20, GRAY) : DrawText("", 30, 110, 20, GRAY);
-                (completedTotal[2]) ? DrawText("-France Completed", 30, 110, 20, GRAY) : DrawText("", 30, 110, 20, GRAY);
-                (completedTotal[3]) ? DrawText("-Spain Completed", 30, 110, 20, GRAY) : DrawText("", 30, 110, 20, GRAY);
-                (completedTotal[4]) ? DrawText("-Bulgaria Completed", 30, 110, 20, GRAY) : DrawText("", 30, 110, 20, GRAY);
+            //Check if any country game is completed
+            (completedTotal[0]) ? DrawText("-Tutorial Completed", 30, 110, 20, GRAY) : DrawText("", 30, 110, 20, GRAY);
+            (completedTotal[1]) ? DrawText("-Italy Completed", 30, 110, 20, GRAY) : DrawText("", 30, 110, 20, GRAY);
+            (completedTotal[2]) ? DrawText("-France Completed", 30, 110, 20, GRAY) : DrawText("", 30, 110, 20, GRAY);
+            (completedTotal[3]) ? DrawText("-Spain Completed", 30, 110, 20, GRAY) : DrawText("", 30, 110, 20, GRAY);
+            (completedTotal[4]) ? DrawText("-Bulgaria Completed", 30, 110, 20, GRAY) : DrawText("", 30, 110, 20, GRAY);
+            break;
 
-                break;
-
-                case GAMEPLAY:
-                switch (countryChoice) {
+            //Check chosen country
+            case GAMEPLAY:
+            switch (countryChoice) 
+            {
                 case 0:
-                    completedTotal[0] = 1;
-                    renderTutorial(&currentScreen, &rabbitPosition, &rabbitDirection, frame, &front, &frontIdle, &frontWalk1, &frontWalk2, &left, &left2, &leftWalk2, &right, &right2, &rightWalk2, &back, &backIdle, &backWalk1, &backWalk2, &tutorialBackground, &goal);
-                    break;
-                    case 1:
-                    completedTotal[1] = 1;
-                    renderItaly(&currentScreen, &rabbitPosition, &rabbitDirection, frame, &front, &left, &left2, &right, &right2, &back);
-                    break;
-                    case 2:
-                    renderFrance(&currentScreen, &isFrench, &isFrenchTalking, &franceBackground, &franceGirlIdle, &franceGirlTalk, &rabbitDetailed, &textbox, &franceDialogue, &firstColor, &secondColor, &franceDialogueCounter, &franceQuestions, &tempCorrect, &completedTotal[2]);
-                    break;
-                    case 3:
-                    completedTotal[3] = 1;
-                    renderSpain(&currentScreen, &rabbitPosition, &rabbitDirection, frame, &front, &left, &left2, &right, &right2, &back);
-                    break;
-                    case 4:
-                    completedTotal[4] = 1;
-                    renderBulgaria(&currentScreen, &rabbitPosition, &rabbitDirection, frame, &front, &frontIdle, &frontWalk1, &frontWalk2, &left, &left2, &leftWalk2, &right, &right2, &rightWalk2, &back, &backIdle, &backWalk1, &backWalk2, &bulgariaBackground, &rose);
-                    break;
-                    default:
-                    currentScreen = LEVELSELECT;
-                    break;
-                    }
+                completedTotal[0] = 1;
+                renderTutorial(&currentScreen, &rabbitPosition, &rabbitDirection, frame, &front, &frontIdle, &frontWalk1, &frontWalk2, &left, &left2, &leftWalk2, &right, &right2, &rightWalk2, &back, &backIdle, &backWalk1, &backWalk2, &tutorialBackground, &goal);
+                break;
+
+                case 1:
+                completedTotal[1] = 1;
+                renderItaly(&currentScreen, &rabbitPosition, &rabbitDirection, frame, &front, &left, &left2, &right, &right2, &back);
+                break;
+
+                case 2:
+                renderFrance(&currentScreen, &isFrench, &isFrenchTalking, &franceBackground, &franceGirlIdle, &franceGirlTalk, &rabbitDetailed, &textbox, &franceDialogue, &firstColor, &secondColor, &franceDialogueCounter, &franceQuestions, &tempCorrect, &completedTotal[2]);
+                break;
+
+                case 3:
+                completedTotal[3] = 1;
+                renderSpain(&currentScreen, &rabbitPosition, &rabbitDirection, frame, &front, &left, &left2, &right, &right2, &back);
+                break;
+
+                case 4:
+                completedTotal[4] = 1;
+                renderBulgaria(&currentScreen, &rabbitPosition, &rabbitDirection, frame, &front, &frontIdle, &frontWalk1, &frontWalk2, &left, &left2, &leftWalk2, &right, &right2, &rightWalk2, &back, &backIdle, &backWalk1, &backWalk2, &bulgariaBackground, &rose);
+                break;
+
+                default:
+                currentScreen = LEVELSELECT;
                 break;
             }
+            break;
+        }
 
+        // End drawing
         EndDrawing();
     }
+
+    //Unload thr map
     unloadMap(map, star);
-    CloseWindow();  // Close window
+
+    // Close window
+    CloseWindow(); 
+    
     return 0;
 }
